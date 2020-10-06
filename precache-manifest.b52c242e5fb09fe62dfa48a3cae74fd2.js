ca419dfccd11e81ee9e8fce7903ee8ac9256b56d
aleksandrov <aaleksandrov@bears.software>
2020-10-07 00:48:13 +0300

    Updates

---
+++ precache-manifest.b52c242e5fb09fe62dfa48a3cae74fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653c8f5c07712aea92f5ea3d956ce179",
+    "url": "/erolife-crm/index.html"
+  },
+  {
+    "revision": "930693d08c0ef5fa157a",
+    "url": "/erolife-crm/static/css/main.b2f8784b.chunk.css"
+  },
+  {
+    "revision": "108d3a6653aa51686550",
+    "url": "/erolife-crm/static/js/2.46b26af8.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/erolife-crm/static/js/2.46b26af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930693d08c0ef5fa157a",
+    "url": "/erolife-crm/static/js/main.f16adb3e.chunk.js"
+  },
+  {
+    "revision": "49531e89d0d65d941d12",
+    "url": "/erolife-crm/static/js/runtime-main.9c942d12.js"
+  }
+]);
