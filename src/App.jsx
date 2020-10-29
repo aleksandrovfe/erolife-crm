dbe3f5ec4fbf79f083ed8627d3b50d16923f1a69
aleksandrov <aaleksandrov@bears.software>
2020-10-30 01:03:04 +0200

    init 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import {db} from "./firebaseConfig";
 function App() {
     const [records, setRecords] = useState([])
 
+    const toDate = seconds => {
+        let date = new Date(null)
+        date.setTime(seconds * 1000)
+        return date.toLocaleString()
+    }
+
     useEffect(() => {
         db.collection('records')
             .orderBy('timestamp', 'desc')
@@ -22,8 +28,10 @@ function App() {
             {records.length ? (
                 records.map(({id, record}) => (
                     <div key={id}>
-                        <p>{record.name}</p>
-                        <p>{record.phone}</p>
+                        <p>Имя {record.name}</p>
+                        <p>Врач или направление {record.service}</p>
+                        <p>Телефон {record.phone}</p>
+                        <p>Время записи {toDate(record.timestamp.seconds)}</p>
                         <br/><br/>
                     </div>
                 ))
